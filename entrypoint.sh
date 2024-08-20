f446063b29d9c21f8e0934cc2b40aa2378400c34
Bryan Baird <BryanBaird@users.noreply.github.com>
2024-08-20 10:16:52 -0400

    Fix typo in error message

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 cd "$APP_DIR/$REPO_PATH_DIR"
 
 if [ ! -f start_service.sh ]; then
-    echo "The required file 'start_service.shy' is not found at $APP_DIR/$REPO_PATH_DIR" >&2
+    echo "The required file 'start_service.sh' is not found at $APP_DIR/$REPO_PATH_DIR" >&2
     exit 1
 fi
 ./start_service.sh
